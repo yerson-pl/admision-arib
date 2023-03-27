1f41668c43c4d5177a2dc63349c1b301015b06b9
yerson-pl <yerson.pl@gmail.com>
2023-03-27 09:56:06 -0500

    Configuracion de ZONA HORARIA

--- main/settings.py
+++ main/settings.py
@@ -18,7 +18,7 @@ from dj_database_url import parse as db_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+#export =LC_ALL
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
