9409fb123de8995094451fb47ccb16de4d6516f0
yerson-pl <yerson.pl@gmail.com>
2023-03-27 09:30:02 -0500

    Configuracion de dominiorailway

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-cb86.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
